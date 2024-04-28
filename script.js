915cdce33443b35daeb04b597b6046a7a8b4d9a0
Devendra kumar <devendrakumar9580387090@gamil.com>
2024-04-28 11:14:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+//for Lopp
+
+
+// for (let i=1; i<=100; i++)
+// {
+//    document.write(`${i} <br/>`)
+// }
+
+    //  Whole loop concept
+    // let i=1;
+    // while(i<=100){
+    //     document.write(`${"colege"} <br/>`);
+    //     i++;
+    // }
+
+    // 3 DO WHILE LOOP
+    // let i=20;
+    // do{
+    //     document.write("apna colege");
+    //     i++
+    
+    // }while (i<=3);
+
+    // 4 FOR OF LOOP
+    // let str ="apna college";
+    // for(let i of str){
+    //     document.write(i);
+    // }
+
+    // 5 FOR IN LOOP 
+    // let starend = {
+    //     name:"Rahul Kumar",
+    //     age:20,
+    //     cgpa:7.7,
+
+    // };
+    // for (let key in starend){
+    //     document.write("key",key,"value",starend[key])
+    // }
+
+    // practice
+    // PRINT ALL EVEN AND ODD NUMBER FROM 0TO 1000
+    // for (let num =0; num<=100; num++)
+    // {
+    //     if(num%2 ===0){
+    //         document.write(num )
+    //     }
+    // }
+
+
+    // HOW TO PRINT ODD NUMBER
+    // for (let num=0; num<=100; num++){
+    //     if(num%2 !=0){
+    //         document.write(num)
+    //     }
+    // }
+       
+    //    Q CREATE A GAME WHERE YOU START WITH ANY RANDOM NUMBER AKE THE 
+    //    USER TO KEEP GUESIING THE GAME NUMBER UNTILE THE USER ENTER CREATE VALUE
+
+    // let gamNum = 25;
+    // let userNum = prompt("guess the right number");
+    // while (userNum !=gamNum){
+    //     userNum = prompt("you enter a wrong number guezz again");
+    // }
+    // document.write("congratulations you enter the right number");
+
+    // let str = "APNA COLLEGE ";
+    // let nesrt= str.toLowerCase();
+    // document.write(nesrt);
+
+    // let str1 ="apna";
+    // let str2 ="college";
+
+    // let res = str1.concat(str2);
+    // document.write(res);
+
+
+   
+    // do some practice
+    // let fullname = prompt("enter your full name without any spaces");
+    // let userName = "@" + fullname + fullname.length;
+    // document.write(userName);
+
+
+    // function example() {
+    //     var a = 5;
+    //     var b = 10;
+    //     if (a === 5) {
+    //       let a = 4;
+    //       var b = 1;
+    //       console.log(a);
+    //       console.log(b);
+    //     }
+    //     console.log(a);
+    //     console.log(b);
+    //   }
+    //   example();
+    // let a='5';
+    // let b=5;
+    // console.log(a==b);
+    // console.log(a===b);
+    // a = 10;
+    
+    // function example() {
+    //    let a = 20;
+    //     console.log(a);
+    //     }
+    //     example();
+        
+    //     console.log(a);
+    
+    
+    //  
+    let x = confirm('Do you confirm?');
+if (x) {
+  console.log('Confirmed');
+} else {
+  console.log('Not confirmed');
+}
